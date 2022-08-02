a83a18d08be9991d44fab818b4159e26f6b5f665
Lora <lospa100@hhu.de>
2022-08-02 23:42:52 +0200

    longest paragraphs

--- paragraph_approach.py
+++ paragraph_approach.py
@@ -7,15 +7,15 @@ from collections import Counter
 # Text in Absätze teilen, die längeren Absätze analysieren und dort die häufigsten Worte ausgeben lassen
 
 def csv_to_paragraphs(filename):
-    """ A function that reads a csv file and separates it into paragraphs.
+    """ A function that reads a csv file, separates it into paragraphs and saves the 10 longest paragraphs in a string.
 
     Parameters
     ----------
-    filename: name of the file in csv format
+    filename: name of the csv-file
 
     Return
     ------
-    manifesto_paragraphs: a list of strings (text chunks separated by headers ("H"))
+    longest_paragraphs: a list of strings (text chunks separated by headers ("H")), representing the 10 longest paragraphs
     """
     with open(filename, newline='', encoding='utf8') as csv_file:
         reader = csv.reader(csv_file)
@@ -26,36 +26,47 @@ def csv_to_paragraphs(filename):
             if line[1] != "H":
                 current_paragraph = current_paragraph + " " + line[0]
             elif line[1] == "H":
-                manifesto_paragraphs.append(current_paragraph)
+                manifesto_paragraphs.append(current_paragraph) #
                 current_paragraph = ""
-        for paragraph in manifesto_paragraphs:
-            if len(paragraph) < 3000:
-                manifesto_paragraphs.remove(paragraph)
-    return manifesto_paragraphs
 
-def lemmatize(manifesto_paragraphs):
+        longest_paragraphs = []
+        for i in range(10):
+            longest_paragraph = max(manifesto_paragraphs, key=len)
+            longest_paragraphs.append(longest_paragraph)
+            manifesto_paragraphs.remove(longest_paragraph)
+
+    return  longest_paragraphs 
+
+
+
+def lemmatize(longest_paragraphs):
     """ A function that lemmatizes the tokens in a given file using spaCy's German model.
 
     Parameters
     ----------
-    manifesto_parargaphs: list of strings (paragraphs)
+    longest paragraphs: list of strings (10 longest paragraphs)
 
     Return
     ------
     paragraphs_lemmatized: list of paragraphs consisting of lists of lowercase, lemmatized words
     """
     nlp = spacy.load("de_core_news_sm")
-    current_paragraph_lemmatized = []
     paragraphs_lemmatized = []
     for paragraph in manifesto_paragraphs:
         manifesto_processed = nlp(paragraph)
+        current_paragraph_lemmatized = []
         for token in manifesto_processed:
             current_paragraph_lemmatized.append(token.lemma_.lower())
 
         paragraphs_lemmatized.append(current_paragraph_lemmatized)
-        current_manifesto_lemmatized = []
+        current_paragraph_lemmatized = []
 
     return paragraphs_lemmatized
+
+punctuation = [",", ".", "!", "?", "-", "_", ":", ";", "--", "-", " –"]
+custom_stop_words = ["der", "die", "das", "für", "grüne", "linke", "afd", "spd", "cdu", "csu", "fdp", "Für", "über", "neu"]
+STOP_WORDS.update(custom_stop_words)
+STOP_WORDS.update(punctuation)
 
 def remove_stopwords(paragraphs_lemmatized):
     """ A function that removes stop words for each paragraph.
@@ -74,9 +85,10 @@ def remove_stopwords(paragraphs_lemmatized):
         for lemma in paragraph:
             if lemma not in STOP_WORDS:
                 current_paragraph_clean.append(lemma)
-            
-            manifesto_clean.append(current_paragraph_clean)
-    
+
+        manifesto_clean.append(current_paragraph_clean)
+        current_paragraph_clean = []
+
     return manifesto_clean
 
 #STOP WORDS aktualisieren
@@ -92,13 +104,23 @@ def most_frequent(manifesto_clean):
     ------
     3 most common words occuring in each paragraph and their frequency
     """   
-    c = Counter(paragraph)
     most_common = []
     for paragraph in manifesto_clean:
+        c = Counter(paragraph)
         #current_most_common = []
         #current_most_common.append(c.most_common(3))
         most_common.append(c.most_common(3))
     return most_common
 
-
-print(most_frequent(remove_stopwords(lemmatize(csv_to_paragraphs('41113_202109.csv')))))
+print('##############################')
+print(most_frequent(remove_stopwords(lemmatize(csv_to_paragraphs('41113_202109.csv'))))) #grün
+print('##############################')
+print(most_frequent(remove_stopwords(lemmatize(csv_to_paragraphs('41223_202109.csv'))))) #linke
+print('##############################')
+print(most_frequent(remove_stopwords(lemmatize(csv_to_paragraphs('41320_202109.csv'))))) #spd
+print('##############################')
+print(most_frequent(remove_stopwords(lemmatize(csv_to_paragraphs('41420_202109.csv'))))) #fdp
+print('##############################')
+print(most_frequent(remove_stopwords(lemmatize(csv_to_paragraphs('41521_202109.csv'))))) #cdu
+print('##############################')
+print(most_frequent(remove_stopwords(lemmatize(csv_to_paragraphs('41953_202109.csv'))))) #afd
